47c8460036fb7cd3606022d63020a412be5e077a
Yuefei <155691003+dyf4256@users.noreply.github.com>
2024-03-12 17:51:06 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'my_activities', to: 'recommendations#my_activities'
-  root to: "categories#index"
+  root to: "pages#home"
 
   get "up" => "rails/health#show", as: :rails_health_check
   
