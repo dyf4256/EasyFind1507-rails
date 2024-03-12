e56def8ba44bf964686eefe9b46f74f7390d46c0
Yuefei Dong <yuefeidong124@gmail.com>
2024-03-12 17:42:20 -0400

    regular controller

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,25 @@
 class RecommendationsController < ApplicationController
+
+  def new
+    @recommendation = Recommendation.new
+  end
+
+  def show
+    @recommendation = Recommendation.find(params[:id])
+    @recommendation.user = current_user
+  end
+
+  def create
+    @recommendation = Recommendation.new(recommendation_params)
+  end
+
+  def update
+
+  end
+
+  private
+
+  def recommendation_params
+    params.require(:recommendation).permit(:status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :recommendations, only: %i[show new create update]
+
 end
