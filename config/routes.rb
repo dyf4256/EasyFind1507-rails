21c7899711e5088805c97a7592f0738bc2a98eff
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-03-14 13:20:15 -0400

    home page and sign in log in screen

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
-
+  resources :categories, only: %i[index]
   resources :recommendations, only: %i[index show new create update]
 
 end
